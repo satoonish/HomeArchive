b091136a60de8556c1c5fc4bee19acf4988894ae
Satsuki <satsuki200214@gmail.com>
2025-02-18 21:40:16 +0900

    Add: アプリヘッダーを追加しました

--- homearchive.client/src/App.vue
+++ homearchive.client/src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <v-app>
+    <AppHeader/>
+    
     <v-main>
-      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts" setup>
-  //
+import AppHeader from './components/AppHeader.vue';
 </script>
---
+++ homearchive.client/src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<template>
+<v-app-bar :elevation="2" color="primary" app>
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-app-bar-title>HomeArchive</v-app-bar-title>
+
+    <!-- Theme switch -->
+    <v-btn @click="changeTheme">
+        <v-icon>{{ themeIcon }}</v-icon>
+    </v-btn>
+</v-app-bar>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { useTheme } from 'vuetify';
+
+const theme = useTheme();
+const themeIcon = ref('mdi-weather-night');
+
+const changeTheme = () => {
+  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark';
+  themeIcon.value = theme.global.current.value.dark ? 'mdi-weather-sunny' : 'mdi-weather-night';}
+</script>
