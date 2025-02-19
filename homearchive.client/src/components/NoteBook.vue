7599bebfbeef052a539cb746266b55e5ba3aa1c1
Satsuki <satsuki200214@gmail.com>
2025-02-19 23:51:43 +0900

    Add: 新規ノートブックのダイアログを追加しました

--- homearchive.client/src/components/NoteBook.vue
+++ homearchive.client/src/components/NoteBook.vue
@@ -5,13 +5,31 @@
         <div class="menuBox">
             <v-btn color="secondary" prepend-icon="mdi-plus">
                 新規作成
+                <v-dialog
+                    v-model="newNoteDialog"
+                    activator="parent"
+                    width="80%">
+                    <v-card>
+                        <v-card-title>新規ノート</v-card-title>
+                        <v-card-text>
+                            <p>タイトル</p>
+                            <v-text-field></v-text-field>
+                            <p>内容</p>
+                            <v-textarea></v-textarea>
+                        </v-card-text>
+                        <v-card-actions>
+                            <v-btn color="primary">保存</v-btn>
+                            <v-btn color="error" @click="newNoteDialog = false">キャンセル</v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-dialog>
             </v-btn>
         </div>
 
         <v-table density="compact">
             <thead>
                 <tr>
-                    <th v-show="IsidVisible">ID</th>
+                    <th>ID</th>
                     <th>登録日時</th>
                     <th class="tableTitle">タイトル</th>
                 </tr>
@@ -41,6 +59,7 @@
 import { ref } from 'vue';
 
 const IsidVisible = ref(false);
+const newNoteDialog = ref(false);
 </script>
 
 <style>
